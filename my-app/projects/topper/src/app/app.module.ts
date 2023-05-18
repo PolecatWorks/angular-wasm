e97d7564e191bea3fd072e005654ffac1fcce4ad
Ben Greene <BenJGreene@gmail.com>
2023-05-18 08:48:12 +0100

    Update material

--- my-app/projects/topper/src/app/app.module.ts
+++ my-app/projects/topper/src/app/app.module.ts
@@ -7,13 +7,13 @@ import { TerryComponent } from './terry/terry.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 import { NavComponent } from './nav/nav.component';
-import {MatCardModule} from '@angular/material/card'
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card'
+import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
